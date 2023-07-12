b7f90c0387d9f8a05fcbc6395ee9da4f024e5dca
Ritwick Das <ritwick35rd@live.com>
2023-07-13 00:44:03 +0530

    - initial commit - made skeleton of a video uploader interface

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+let file: Ref<null> | null = null;
+let videoUploaded = ref(false);
+
+const videoInput = ref(null);
+
+const selectFile = (event: any) => {
+  if (event.target && event.target.files) {
+    file = event.target.files[0];
+    console.log(file);
+    videoUploaded.value = true;
+  }
+};
+const sendFile = () => {
+  console.log("sending file to the server");
+};
+</script>
+
+<template>
+  <main>
+    <h2>Click Here to upload a video</h2>
+    <form enctype="multipart/form-data" @submit.prevent="sendFile">
+      <label for="video">Upload Video</label>
+      <input type="file" ref="videoInput" @change="selectFile" />
+      <button :disabled="!videoUploaded" class="btn btn-primary">Submit</button>
+    </form>
+  </main>
+</template>
